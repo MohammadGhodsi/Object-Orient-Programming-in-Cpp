03c6ca830c29682f18900bf42a92f39e54df1f51
Mohammad Ghodsi <gsf350mail@gmail.com>
2025-08-12 14:21:09 +0330

    Create Tire_and_Car.cpp

---
+++ Tire_and_Car.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Tire {
+
+    private:
+        
+        int* width;
+        int* aspect_ration;
+        int* diameter;
+
+    public:
+        Tire();
+        Tire(int,int,int);
+        ~Tire();
+        
+
+}
