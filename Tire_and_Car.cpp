ea093bbe6e5aaa06b50f2dc8e2be891c9673fde8
Mohammad Ghodsi <gsf350mail@gmail.com>
2025-08-12 15:48:02 +0330

    Update Tire_and_Car.cpp

--- Tire_and_Car.cpp
+++ Tire_and_Car.cpp
@@ -6,13 +6,36 @@ class Tire {
     private:
         
         int* width;
-        int* aspect_ration;
+        int* aspect_ratio;
         int* diameter;
 
     public:
         Tire();
         Tire(int,int,int);
         ~Tire();
-        
+        int getwidth();
+        int getaspectratio();
+        int getdiamter();
+
+
+    void PrintTire()
+    {
+
+        cout << "Width :" << *width << "Aspect ratio :" << *aspect_ratio << "Diameter" << *diameter;
+
+    }
+};
+
+Tire::Tire()
+{
+    width = 0;
+    aspect_ratio = 0;
+    diameter = 0;
+}
+
+Tire::Tire(int w, int a, int d)
+{
+
+   this->width = new int;
 
 }
