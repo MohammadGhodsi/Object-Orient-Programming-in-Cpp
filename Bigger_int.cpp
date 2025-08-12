0d507fd169d7d949f3efa1ec22c3bc00499db376
Mohammad Ghodsi <gsf350mail@gmail.com>
2025-08-12 11:45:22 +0330

    Update Bigger_int.cpp

--- Bigger_int.cpp
+++ Bigger_int.cpp
@@ -1,4 +1,4 @@
-include<iostream>
+#include <iostream>
 using namespace std;
 
 class BiggerInt {
@@ -21,9 +21,83 @@ class BiggerInt {
     int getint_lenght_();
     int& operator[](int);
 
-    BiggerInt 
+    BiggerInt operator++(); // for pre increasing
+    BiggerInt operator--(); // for pre decreasing
+    BiggerInt operator++(int); //  for post increasing
+    BiggerInt operator--(int); // for post decreasing
+
+};
+
+BiggerInt::BiggerInt () 
+
+{
+
+    big_int_ = nullptr;
+    int_lenght_ = 0;
 
 }
+
+BiggerInt::BiggerInt(const int* obj, int size)
+
+{
+
+    int_lenght_ = size;
+    big_int_ = new int[int_lenght_];
+    for (int i=0; i < int_lenght_; i++)
+    {
+
+        big_int_[i] = obj[i];
+
+    }
+
+}
+
+BiggerInt::BiggerInt(const BiggerInt& obj)
+{
+
+    int_lenght_ = obj.int_lenght_;
+    big_int_ = new int[obj.int_lenght_];
+
+    for (int i = 0; i < int_lenght_; i++)
+    {
+        big_int_[i] = obj.big_int_[i];
+    }
+        
+
+}
+
+    BiggerInt BiggerInt::operator=(const BiggerInt& c)
+{
+    if (this == &c)
+    {
+        return *this;
+    }
+
+    if (big_int_ != nullptr)
+    {
+        delete[] big_int_;
+    }
+
+    int_lenght_ = c.int_lenght_;
+    big_int_ = new int[int_lenght_]
+        
+    for (int i = 0; i < int_lenght_; i++)
+    {
+        big_int_[i] = c.big_int_[i];
+    }
+
+    return *this;
+        
+}
+
+BiggerInt::~BiggerInt()
+{
+    delete[] big_int_;
+}
+
+
+
+
 
 
 
