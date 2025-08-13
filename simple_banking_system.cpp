7c09c1862d479961473cbfafbea4a11dd4c80752
unknown <gsf350mail@gmail.com>
2025-08-13 12:39:07 +0330

    Update simple_banking_system.cpp

--- simple_banking_system.cpp
+++ simple_banking_system.cpp
@@ -1,1 +1,48 @@
-dasdsa
+#include <iostream>
+using namespace std;
+
+class Account
+{
+private:
+    int AN, AB;
+public:
+   
+    int getAN()
+    {
+        return AN;
+    }
+    int getAB()
+    {
+        return AB;
+    }
+    void setAN(int acn)
+    {
+        AN = acn;
+    }
+    void setAB(int acb)
+    {
+        AB = acb;
+    }
+    virtual void Print()
+    {
+        cout << "Account Number : " << AN << endl << "Account Balance :" << AB << endl;
+    }
+
+    virtual void Credit()
+    {
+
+    }
+    virtual void Debit()
+    {
+
+    }
+
+    virtual ~Account() 
+    {
+        cout << "Account deleted!" << endl;
+    }
+
+};
+
+
+
