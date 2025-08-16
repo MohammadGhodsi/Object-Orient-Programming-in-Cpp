b40d8621883cf02c1997f308ba55fe50a49dd9b3
unknown <gsf350mail@gmail.com>
2025-08-16 15:06:42 +0330

    Create shifting_column.cpp

---
+++ shifting_column.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+
+T range(T* array , int rows , int columns)
+{
+    T min = array[0][0];
+    T max = array[0][0];
+
+
+    for (int i=0;i< rows;++i)
+    {
+        for (int j=0; j<columns;++j)
+        {
+            if(array[i][j])
+        }
+    }
+
+};
+
+
+
+
+
+
+
