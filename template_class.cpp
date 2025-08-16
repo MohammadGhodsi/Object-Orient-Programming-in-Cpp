df9c71ab343c9db66f2995040b409c08c6590a4f
unknown <gsf350mail@gmail.com>
2025-08-16 14:06:29 +0330

    Update template_class.cpp

--- template_class.cpp
+++ template_class.cpp
@@ -4,6 +4,29 @@ using namespace std;
 
 template <class T , int N>
 
+class Sequence 
+{
+    private :
+        T memblock[N];
+
+    public :
+
+       void setmember(int x, T value)
+       {
+
+            memblock[x] = value;
+       }
+
+       T getmember(int x)
+       {
+        return memblock[x];
+       }
+
+
+};
+
+
+
 
 
 
